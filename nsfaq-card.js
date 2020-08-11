9caf5c4476a1c240b706b56461b6ebc8af6db817
John Tubert <jtubert@hotmail.com>
2020-08-11 10:55:15 -0400

    add 24 point font medium

--- nsfaq-card.js
+++ nsfaq-card.js
@@ -4,11 +4,12 @@ require('dotenv').config();
 runLocal();
 
 async function runLocal(){
-  const holder = await entrypoint({queryStringParameters:{category: "category", color: "FF0000", text: "What is your name?"}});
+  const event = {queryStringParameters:{category: "category", color: "FF0000", text: "What is your name?"}};
+  const holder = await entrypoint(null);
 
-  const image = await holder.writeAsync("./resized/test2.png");
+  const image = await holder.writeAsync("./cards/test.png");
 
-  console.log("Saved in " + "./resized/test.png");
+  console.log("Saved in " + "./cards/test.png");
 }
 
 //curl -H "Accept: image/png" https://u9pwl8bl3i.execute-api.us-east-1.amazonaws.com/dev/nsfaq-card > card.png
@@ -56,23 +57,22 @@ async function entrypoint(event){
 }
 
 //CONVERT FONT
-
 //https://ttf2fnt.com/
+
+//Jimp.FONT_SANS_32_WHITE);
+//https://github.com/oliver-moran/jimp/tree/master/packages/plugin-print/fonts
 
 async function createCard(category, color, text){
   const w = 564;
   const h = 789;
-  const left = 30;
-  const top = 30;
+  const left = 60;
+  const top = 60;
   const holder = await new Jimp(w, h, color);
-  //Jimp.FONT_SANS_32_WHITE);
-  //https://github.com/oliver-moran/jimp/tree/master/packages/plugin-print/fonts
-  //const font32 = await Jimp.loadFont("https://raw.githubusercontent.com/oliver-moran/jimp/master/packages/plugin-print/fonts/open-sans/open-sans-64-white/open-sans-64-white.fnt");
-  //const font16 = await Jimp.loadFont("https://raw.githubusercontent.com/oliver-moran/jimp/master/packages/plugin-print/fonts/open-sans/open-sans-16-white/open-sans-16-white.fnt");
-  const font32 = await Jimp.loadFont("https://raw.githubusercontent.com/jtubert/serverless-slack-image/master/fonts/HelveticaNeueLTStd-Hv.ttf.fnt");
-  //const font16 = await Jimp.loadFont("https://raw.githubusercontent.com/jtubert/serverless-slack-image/master/fonts/HelveticaNeueLTStd-Hv.-24-white.fnt");
-  holder.print(font32, left, top, text, w-(left*2));
-  //holder.print(font16, left, 700, category, w-(left*2));
+  
+  const font24 = await Jimp.loadFont("https://raw.githubusercontent.com/jtubert/serverless-slack-image/master/fonts/white-24/HelveticaNeueLTStd-Hv.ttf.fnt");
+  const font50 = await Jimp.loadFont("https://raw.githubusercontent.com/jtubert/serverless-slack-image/master/fonts/white-50/HelveticaNeueLTStd-Hv.ttf.fnt");
+  holder.print(font50, left, top, text, w-(left*2));
+  holder.print(font24, left, 700, category, w-(left*2));
   return holder;
 }
 
